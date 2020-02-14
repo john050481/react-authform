e40b6bdace7c353873099d02612d4ed79344af10
john050481 <john0504@yandex.ru>
2020-02-14 23:41:08 +0300

    rename

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Auth from './AuthForm/AuthForm';
+import AuthForm from './AuthForm/AuthForm';
 
 test('renders Auth', () => {
-  const { getByText } = render(<Auth />);
+  const { getByText } = render(<AuthForm />);
   const linkElement = getByText(/Your Website/i);
   expect(linkElement).toBeInTheDocument();
 });
